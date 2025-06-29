b18ae0615eb06fe45271c11a0edb844202d435ee
Caua Henrique <cauamh321@gmail.com>
2025-06-28 23:37:53 -0300

    criando arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import './styles/theme.css'
+import './styles/global.css'
+
+
 export function App() {
   console.log('oi');
 
   return (
-    <div>
+    <>
       <h1>Ola mundo (do app)</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
@@ -10,7 +14,7 @@ export function App() {
         maiores rerum tempora natus tempore optio enim ea quaerat ratione
         voluptatem?
       </p>
-    </div>
+    </>
   );
 }
 
