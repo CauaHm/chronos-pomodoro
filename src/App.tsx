cc5d32c50ebcbb911ca7896646b10a72e339fdab
Caua Henrique <cauamh321@gmail.com>
2025-07-19 22:44:56 -0300

    Some changeds

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,7 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-        <div className='container'>
-          <div className='content'>
-            <section>
-              Logo
-            </section>
-          </div>
-        </div>
-         <div className='container'>
-          <div className='content'>
-            <section>
-              Menu
-            </section>
-          </div>
-        </div>
-         <div className='container'>
-          <div className='content'>
-            <section>
-              Form
-            </section>
-          </div>
-        </div>
-         <div className='container'>
-          <div className='content'>
-            <section>
-              Footer
-            </section>
-          </div>
-        </div>
+      
     </>
   );
 }
