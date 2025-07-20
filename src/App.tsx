6f10e02b56ca4e57eb75450cff2d824555b557d3
Caua Henrique <cauamh321@gmail.com>
2025-07-20 17:27:12 -0300

    crated menu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
       </Container>
     </>
   );
