c126dc4538b034b675d99da05a421006d3db3339
Caua Henrique <cauamh321@gmail.com>
2025-07-28 00:10:03 -0300

    code refactor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,12 @@
 import styles from './styles.module.css';
 
 export function Footer() {
-  return <footer className={styles.footer}>
-    <a href="#">Entenda como funciona a técnica pomodoro🍎</a>
-    <a href="#">Chronos Pomodoro &copy; {new Date().getFullYear()} - feito com 💚</a>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <a href='#'>Entenda como funciona a técnica pomodoro🍎</a>
+      <a href='#'>
+        Chronos Pomodoro &copy; {new Date().getFullYear()} - feito com 💚
+      </a>
+    </footer>
+  );
 }
