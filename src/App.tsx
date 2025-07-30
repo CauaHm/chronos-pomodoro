bf97a9eec7a1fad9b5c6e6bc96ca76bd4e25031e
Caua Henrique <cauamh321@gmail.com>
2025-07-30 16:19:42 -0300

    some changeds

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,25 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
+import { useState } from 'react';
+import type { TaskStateModels } from './models/TaskStateModels';
+
+const initialState: TaskStateModels = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsReamining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  }
+  
+}
 
 export function App() {
+  const [state, setState] = useState(initialState)
   return (
     <>
       <Home />
---
+++ src/models/TaskModels.ts
@@ -0,0 +1,11 @@
+import type { TaskStateModels } from "./TaskStateModels";
+
+export type TaskModels = {
+  id: string;
+  name: string;
+  duration: number;
+  startDate: number;
+  completeDate: number | null;
+  interruptDate: number | null;
+  type: keyof TaskStateModels['config'];
+};
